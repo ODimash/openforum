80fef21ca42b03345414d08986bc0ed3f35aef3e
odimash <dimasosmanov5@gmail.com>
2024-09-07 12:06:00 +0500

    remove method CommentRepository.findByTopic()

--- src/main/java/odimash/openforum/domain/repository/CommentRepository.java
+++ src/main/java/odimash/openforum/domain/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package odimash.openforum.domain.repository;
 
 import odimash.openforum.domain.entity.Comment;
-import odimash.openforum.domain.entity.Topic;
 import odimash.openforum.domain.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
